03d334369166398332fdeb978a32641d9c39b4fa
Alishba727 <an0906670@gmail.com>
2026-02-24 14:04:04 +0500

    datatypes file

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+#Numeric
+integer=7686,-878987
+float=890.68,676.099
+#text
+string="we are learning data types"
+
+list:cloud_class_student=["ali","hamza"]
+tuple=cloud_class=("teachers","students")
+dictionary=student_data={'name':"ali",'age':"22"}
+set={12,23,45}
+name = "Alishba"
+age = 20
+marks = 95.5
+colors = ("blue", "green", "red")
+numbers = {1, 2, 3, 4}
+student = {"name": "alishba", "age": 22}
+items = ["pen", "sharpner", "scale"]
+
+print(name, age, marks, colors, numbers, student, items)

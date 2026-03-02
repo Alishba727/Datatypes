2562602dc22c76df0a8f2f7c237c01c0eae8a983
Alishba727 <an0906670@gmail.com>
2026-03-02 14:11:30 +0500

    basics

--- if.else/basics/ifelse.py
+++ if.else/basics/ifelse.py
@@ -4,5 +4,10 @@ if marks>=80:
     print("congratulations you are passed")
 else:print("sorry you are failed")
 
+age = 15
+if age >= 18:
+    print("You can vote.")
+else:
+    print("Too young to vote.")
 
 
